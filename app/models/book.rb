9f6413b85f3795dec7a1942d701fa33437f853b1
Kohei Higashi <112879983+koheiino@users.noreply.github.com>
2023-05-02 06:28:03 +0000

    [Add]Bookモデルに必要な設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  # 画像を持たす設定
+  has_one_attached :image
+  # リレーション
+  belongs_to :user
 end
--- db/migrate/20230502061523_create_books.rb
+++ db/migrate/20230502061523_create_books.rb
@@ -1,7 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-
+      t.string :title
+      t.text :body
+      t.text :option
       t.timestamps
     end
   end
