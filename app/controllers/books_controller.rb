2503866f1c1169cf60c6e9a684437856c385c9e1
Kohei Higashi <112879983+koheiino@users.noreply.github.com>
2023-05-03 13:18:51 +0000

    [update]signupエラー修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,17 @@ class BooksController < ApplicationController
   def about
     
   end
+  
+  def new
+    @list = List.new
+  end
+  def create
+    list = List.new(book_params)
+    list.save
+    redirect_to'/top'
+  end
+
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
 end
