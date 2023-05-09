e87683a94f47350d8b716dce3cddac5d1f0e1e82
Kohei Higashi <112879983+koheiino@users.noreply.github.com>
2023-05-09 02:40:05 +0000

    [update]ログイン不具合修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,25 @@ class UsersController < ApplicationController
     @book = Book.new
     @books = Book.all
   end
-  
+
+  def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+        render "edit"
+    else
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="You have updated user successfully."
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
   private
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'devise/sessions',
+    registrations: 'devise/registrations'
+  }
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :post_images, only: [:new, :index, :show]
+  resources :users
   resources :books
   resources :homes
   get '/about', to: 'books#about', as: 'about'
